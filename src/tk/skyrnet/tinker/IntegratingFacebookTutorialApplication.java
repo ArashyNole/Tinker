964cdc14c6266274911f4268b7f67a252b5ca762
Todd Ryks <ryks@cs.fsu.edu>
2014-07-16 03:37:59 -0400

    Used Facebook integration example from Parse tutorial

---
+++ src/tk/skyrnet/tinker/IntegratingFacebookTutorialApplication.java
@@ -0,0 +1,23 @@
+package tk.skyrnet.tinker;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
+public class IntegratingFacebookTutorialApplication extends Application {
+
+	static final String TAG = "MyApp";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Parse.initialize(this, "1CYzOY4wtioVdMlVPLNKSuzWykXBd0rzApjwR7vR", "Qr3znHel8h6g7lFMGBcku6awyebqlMLICcg4Zww1");
+
+		// Set your Facebook App Id in strings.xml
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+
+	}
+
+}
